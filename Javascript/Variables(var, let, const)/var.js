bad80bf38355b41e7437a735860ead359fa29347
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-02 14:44:39 +0530

    var- functional scope

--- Javascript/Variables(var, let, const)/var.js
+++ Javascript/Variables(var, let, const)/var.js
@@ -1,13 +1,13 @@
 /* "var" is functional scope.
-  because we can access "var variables inside {}." */
+  because we can access "var variables" outside of the block. */
 
-var a = 10;
-var b = 20;
+
 
 {
-console.log(a);
-console.log(b);
+var a = 10;
+var b = 20;
 }
 
-
+console.log(a);
+console.log(b);
 

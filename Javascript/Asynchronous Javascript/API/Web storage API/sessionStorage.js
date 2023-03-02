1636ed8c64077ff2094bf013a23e5469d34a57c5
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-02 11:33:38 +0530

    session Storage and storage event

---
+++ Javascript/Asynchronous Javascript/API/Web storage API/sessionStorage.js
@@ -0,0 +1,5 @@
+/*SessionStorage exists only within the current browser, tab another tab with same page will have different storage. 
+   When refresh the page data is there but when closed or reopen the tab data is not there.*/
+
+// storage event: when the data gets updated in localstorage or sessionstorage, storage event trigger with these properties.
+
